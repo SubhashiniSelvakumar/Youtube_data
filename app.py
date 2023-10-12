a391bf9da6187218a07ebd5db448efdf223a8a00
SubhashiniSelvakumar <146129745+SubhashiniSelvakumar@users.noreply.github.com>
2023-10-12 09:18:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,6 +32,9 @@ st.markdown(custom_css, unsafe_allow_html=True)
 # Mongo Db Connection
 client = pymongo.MongoClient("mongodb+srv://subhashini:briyani@cluster0.neuug1f.mongodb.net/?retryWrites=true&w=majority")
 db = client['youtube_data']
+channel_details_collection = db['channel_details']
+comments_details_collection = db['comments_details']
+video_details_collection = db['video_details']
 
 # CONNECTING WITH MYSQL DATABASE
 mydb = mysql.connector.connect(
@@ -266,106 +269,123 @@ elif selected == "Transform":
     ch_names = channel_names()
     user_inp = st.selectbox("Select channel", options=ch_names)
     
-    def insert_into_channels():
-        collections = db.channel_details
-        query = """INSERT INTO channels VALUES(%s,%s,%s,%s,%s,%s,%s,%s)"""
-        
-        for i in collections.find({"Channel_name" : user_inp}, {'_id': 0}):
-            mycursor.execute(query, tuple(i.values()))
-            mydb.commit()
-        
-    def insert_into_videos():
-        collectionss = db.video_details
+    def insert_into_channels(user_inp):
+        collections1 = db.channel_details
+        query = """INSERT INTO channels (Channel_id, Channel_name, Playlist_id, Subscribers, Views, Total_videos, Description, Country) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"""
+    
+        for doc in collections1.find({"Channel_name" : user_inp}, {'_id': 0}):
+           data_to_insert = (
+            doc["Channel_id"],
+            doc["Channel_name"],
+            doc["Playlist_id"],
+            doc["Subscribers"],
+            doc["Views"],
+            doc["Total_videos"],
+            doc["Description"],
+            doc["Country"]
+        )
+        mycursor.execute(query, data_to_insert)
+        mydb.commit()
+
+    def insert_into_videos(user_inp):
+        collections2 = db.video_details
         mycursor.execute("""
-            SELECT channel_name AS Channel_Name
-            FROM videos
-            WHERE Published_date LIKE '2022%'
-            GROUP BY channel_name
-            ORDER BY channel_name
-        """)
+        SELECT channel_name AS Channel_Name
+        FROM videos
+        WHERE Published_date LIKE '2022%'
+        GROUP BY channel_name
+        ORDER BY channel_name
+     """)
+       
+    # def insert_into_videos():
+    #     collections = db.video_details
+    #     mycursor.execute("""
+    #         SELECT channel_name AS Channel_Name
+    #         FROM videos
+    #         WHERE Published_date LIKE '2022%'
+    #         GROUP BY channel_name
+    #         ORDER BY channel_name
+    #     """)
 
-        query1 = """INSERT INTO videos VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+        query1 = """INSERT INTO videos (Video_id, Channel_name, Title, Likes, Views) VALUES (%s,%s,%s,%s,%s,%s)"""
 
-        for i in collectionss.find({"Channel_name" : user_inp}, {"_id": 0}):
-            t = tuple(i.values())
-            mycursor.execute(query1, t)
-            mydb.commit()
+        for doc in collections2.find({"Channel_name" : user_inp}, {"_id": 0}):
+           data_to_insert = (
+            doc["Video_id"],
+            doc["Channel_name"],
+            doc["Title"],
+            doc["Likes"],
+            doc["Views"]
+        )
+        mycursor.execute(query1, data_to_insert)
+        mydb.commit()
 
-    def insert_into_comments():
+    def insert_into_comments(user_inp):
         collections1 = db.video_details
         collections2 = db.comments_details
-        query2 = """INSERT INTO comments VALUES(%s,%s,%s,%s,%s,%s,%s)"""
+        query2 = """INSERT INTO comments (Comment_id, Video_id, Comment_text, Comment_author, Comment_posted_date, Like_count, Reply_count) VALUES (%s,%s,%s,%s,%s,%s,%s)"""
 
         for vid in collections1.find({"Channel_name" : user_inp}, {'_id' : 0}):
-            for i in collections2.find({'Video_id': vid['Video_id']}, {'_id' : 0}):
-                t = tuple(i.values())
-                mycursor.execute(query2, t)
-                mydb.commit()
+            for doc in collections2.find({'Video_id': vid['Video_id']}, {'_id' : 0}):
+               data_to_insert = (
+                doc["Comment_id"],
+                doc["Video_id"],
+                doc["Comment_text"],
+                doc["Comment_author"],
+                doc["Comment_posted_date"],
+                doc["Like_count"],
+                doc["Reply_count"]
+             )
+            mycursor.execute(query2, data_to_insert)
+            mydb.commit()
 
     if st.button("Submit"):
         try:
-            insert_into_channels()
-            insert_into_videos()
-            insert_into_comments()
-            st.success("Transformation to MySQL Successful!!!")
+           insert_into_channels(user_inp)
+           insert_into_videos(user_inp)
+           insert_into_comments(user_inp)
+           st.success("Transformation to MySQL Successful!!!")
         except:
-            st.error("Channel details already transformed!!")
+             st.error("Channel details already transformed!!")
 
 # View page
 elif selected == "View":
     st.write("## :orange[Select any question to get Insights]")
-    questions = st.selectbox('Questions',
-    ['Click the question that you would like to query',
-    '1. What are the names of all the videos and their corresponding channels?',
-    '2. Which channels have the most number of videos, and how many videos do they have?',
-    '3. What are the top 10 most viewed videos and their respective channels?',
-    '4. How many comments were made on each video, and what are their corresponding video names?',
-    '5. Which videos have the highest number of likes, and what are their corresponding channel names?',
-    '6. What is the total number of likes and dislikes for each video, and what are their corresponding video names?',
-    '7. What is the total number of views for each channel, and what are their corresponding channel names?',
-    '8. What are the names of all the channels that have published videos in the year 2022?',
-    '9. What is the average duration of all videos in each channel, and what are their corresponding channel names?',
-    '10. Which videos have the highest number of comments, and what are their corresponding channel names?'])
-    
+    questions = st.selectbox('Questions', [
+        'Click the question that you would like to query',
+        '1. What are the names of all the videos and their corresponding channels?',
+        '2. Which channels have the most number of videos, and how many videos do they have?',
+        '3. What are the top 10 most viewed videos and their respective channels?',
+        '4. How many comments were made on each video, and what are their corresponding video names?',
+        '5. Which videos have the highest number of likes, and what are their corresponding channel names?',
+        '6. What is the total number of likes and dislikes for each video, and what are their corresponding video names?',
+        '7. What is the total number of views for each channel, and what are their corresponding channel names?',
+        '8. What are the names of all the channels that have published videos in the year 2022?',
+        '9. What is the average duration of all videos in each channel, and what are their corresponding channel names?',
+        '10. Which videos have the highest number of comments, and what are their corresponding channel names?'
+    ])
+
     if questions == '1. What are the names of all the videos and their corresponding channels?':
         mycursor.execute("""SELECT title AS Video_Title, channel_name AS Channel_Name FROM videos ORDER BY channel_name""")
-        
-        df = pd.DataFrame(mycursor.fetchall(),columns=mycursor.column_names)
+        df = pd.DataFrame(mycursor.fetchall(), columns=mycursor.column_names)
         st.write(df)
-        
+
     elif questions == '2. Which channels have the most number of videos, and how many videos do they have?':
-        mycursor.execute("""SELECT channel_name 
-        AS Channel_Name, total_videos AS Total_Videos
-                            FROM channels
-                            ORDER BY total_videos DESC""")
-        df = pd.DataFrame(mycursor.fetchall(),columns=mycursor.column_names)
+        mycursor.execute("""SELECT channel_name AS Channel_Name, total_videos AS Total_Videos FROM channels ORDER BY total_videos DESC""")
+        df = pd.DataFrame(mycursor.fetchall(), columns=mycursor.column_names)
         st.write(df)
         st.write("### :green[Number of videos in each channel :]")
-        #st.bar_chart(df,x= mycursor.column_names[0],y= mycursor.column_names[1])
-        fig = px.bar(df,
-                     x=mycursor.column_names[0],
-                     y=mycursor.column_names[1],
-                     orientation='v',
-                     color=mycursor.column_names[0]
-                    )
-        st.plotly_chart(fig,use_container_width=True)
-        
+        fig = px.bar(df, x=mycursor.column_names[0], y=mycursor.column_names[1], orientation='v', color=mycursor.column_names[0])
+        st.plotly_chart(fig, use_container_width=True)
+
     elif questions == '3. What are the top 10 most viewed videos and their respective channels?':
-        mycursor.execute("""SELECT channel_name AS Channel_Name, title AS Video_Title, views AS Views 
-                            FROM videos
-                            ORDER BY views DESC
-                            LIMIT 10""")
-        df = pd.DataFrame(mycursor.fetchall(),columns=mycursor.column_names)
+        mycursor.execute("""SELECT channel_name AS Channel_Name, title AS Video_Title, views AS Views FROM videos ORDER BY views DESC LIMIT 10""")
+        df = pd.DataFrame(mycursor.fetchall(), columns=mycursor.column_names)
         st.write(df)
         st.write("### :green[Top 10 most viewed videos :]")
-        fig = px.bar(df,
-                     x=mycursor.column_names[2],
-                     y=mycursor.column_names[1],
-                     orientation='h',
-                     color=mycursor.column_names[0]
-                    )
-        st.plotly_chart(fig,use_container_width=True)
-        
+        fig = px.bar(df, x=mycursor.column_names[2], y=mycursor.column_names[1], orientation='h', color=mycursor.column_names[0])
+        st.plotly_chart(fig, use_container_width=True)
+  
     elif questions == '4. How many comments were made on each video, and what are their corresponding video names?':
         mycursor.execute("""SELECT a.video_id AS Video_id, a.title AS Video_Title, b.Total_Comments
                             FROM videos AS a
